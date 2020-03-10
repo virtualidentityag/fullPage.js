45c52e33a3fb173abca0aff863b6d744047d7e22
Lukas Müller <lukas.mueller@virtual-identity.net>
2020-03-10 11:42:28 +0100

    fix: 🐛 add remove() polyfill

--- dist/fullpage.js
+++ dist/fullpage.js
@@ -1,3 +1,22 @@
+// Element.remove() polyfill
+(function (arr) {
+    arr.forEach(function (item) {
+      if (item.hasOwnProperty('remove')) {
+        return;
+      }
+      Object.defineProperty(item, 'remove', {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: function remove() {
+          if (this.parentNode === null) {
+            return;
+          }
+          this.parentNode.removeChild(this);
+        }
+      });
+    });
+  })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 /*!
  * fullPage 3.0.8
  * https://github.com/alvarotrigo/fullPage.js
